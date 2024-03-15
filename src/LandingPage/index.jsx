d31c23f369153df52f06712e6c63db220204243d
Alejandro Daniel Cruz <daniel_719021@hotmail.es>
2024-03-15 15:19:11 -0600

    errors

--- src/LandingPage/index.jsx
+++ src/LandingPage/index.jsx
@@ -1,23 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Card from "../components/card/Card.component.jsx";
 
-import useNewRequest from "../api/Request.jsx";
 
 import "./index.css";
 
 const LandingPage = () => {
-  let category = "business";
-  const notices = useNewRequest(category);
+
+  const [notices, setNotices] = useState([]);
+  const [category, setCategory] = useState('business');
+  const api = `https://api.bing.microsoft.com/v7.0/news/?category=${category}&count=50&mkt=en-us`;
+  const apiKey = '6b55db0244f24a4f8f0823139d95c95d';
+
+  const fetchNews = async () => {
+    const request = await axios.get(api, {
+      headers: {
+        "Ocp-Apim-Subscription-Key": apiKey,
+      }
+    });
+    const data = request.data;
+    setNotices(data.value);
+  }
+
+  useEffect(() => {
+    fetchNews();
+  }, [category]);
 
   const handleClick = () => {
-    category = "sports";
+    setCategory('sports');
   };
 
   return (
-    <div className="notices-section">
-      <button onClick={handleClick} />
-      {notices.map((notice) => {
-        return <Card key={notice.name} notice={notice} />;
-      })}
+    <div>
+      <button onClick={handleClick} children="Click" />
+      <div className="notices-section">
+        {notices.map((notice) => {
+          return <Card key={notice.name} notice={notice} />;
+        })}
+      </div>
     </div>
   );
 };
